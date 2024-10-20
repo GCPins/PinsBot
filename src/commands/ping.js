6f67072e16c77155b9b1199c506368a0e62daf40
GC; Pins <spot.meow.meow@gmail.com>
2024-10-20 14:49:38 -0400

    ping pong

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -17,36 +17,47 @@ module.exports = class PingCommands extends SlashCommand {
 
   async run(ctx) {
 
-        await ctx.defer();
+        await ctx.defer(true);      
 
-        console.log(client.ws.shards.get(0));
+        let total = 0;
 
-        let ping = client.ws.shards.get(0).ping;
-        let symbol = ping <= 0 ? ":exclamation:" : ping < 100 ? ":green_square:" : ping < 200 ? ":orange_square:" : ":red_square:";
-
-        let msg = `${symbol} API Latency is **${(ping)}**ms ${symbol}`;
+        let ping = client.ws.shards.get(0).ping; // client.ws.ping  would also work
+        let symbol = ping < 0 ? ":exclamation:" : ping < 100 ? ":green_square:" : ping < 200 ? ":orange_square:" : ":red_square:";
+        let msg = `${symbol} API Latency is **${(ping)}**ms ${symbol}`; 
+        total += ping;
 
         let teste = new Discord.EmbedBuilder()
-            .setTitle(":ping_pong: PONG :ping_pong:")
+            .setTitle(":ping_pong:  PONG  :ping_pong:")
             .setTimestamp()
             .setColor('Random')
             .setDescription(`${msg}\nCalculating Client ping... (please wait)`);
 
-        //const prev = await ctx.send(`${msg}\nCalculating Client ping... (please wait)`);
+        //const prev = await ctx.send(`${msg}\nCalculating Client ping... (please wait)`, {  });
 
-        const prev = await ctx.send("", { embeds: [ teste ] });
+        const prev = await ctx.send("", { embeds: [ teste ], ephemeral: true });
+        
+        ping = Math.floor(Date.now() - ctx.invokedAt);
+        symbol = ping <= 0 ? ":exclamation:" : ping < 500 ? ":green_square:" : ping < 750 ? ":orange_square:" : ":red_square:";
+        msg += `\n${symbol} Bot Latency is **${ping}**ms ${symbol}`;
+        total += ping;
 
-        ping = Math.floor(prev.timestamp - ctx.invokedAt);
-        symbol = ping <= 0 ? ":exclamation:" : ping < 100 ? ":green_square:" : ping < 200 ? ":orange_square:" : ":red_square:";
-        msg += `\n${symbol} Client Latency is **${ping}**ms ${symbol}`;
+        ping = prev.timestamp - ctx.invokedAt;
+        symbol = ping <= 0 ? ":exclamation:" : ping < 200 ? ":green_square:" : ping < 450 ? ":orange_square:" : ":red_square:";
+        msg += `\n${symbol} Message Latency is **${ping}**ms ${symbol}`;
+        total += ping;
+
+        msg += '\n';
+
+        symbol = total <= 0 ? ":exclamation:" : total < 700 ? ":green_square:" : total < 900 ? ":orange_square:" : ":red_square:";
+        msg += `\n${symbol} __TOTAL__: **${total}**ms ${symbol}`; 
 
         teste = new Discord.EmbedBuilder()
-            .setTitle(":ping_pong: PONG :ping_pong:")
+            .setTitle(":ping_pong:  PONG  :ping_pong:")
             .setTimestamp()
             .setColor('Random')
             .setDescription(`${msg}`);
 
-        return ctx.editOriginal({embeds: [ teste ]});
-
+        return ctx.editOriginal({embeds: [ teste ], ephemeral: true});
+        //return ctx.editOriginal("Goodbye!", { ephemeral: true });
   }
 }
